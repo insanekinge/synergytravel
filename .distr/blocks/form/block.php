c588904506e378e6dd8c755bb8b835bbafb8f741
Vladislav Kinge <VKinge@synergy.ru>
2025-09-18 16:31:30 +0300

    ID-3 add footer

---
+++ .distr/blocks/form/block.php
@@ -0,0 +1,26 @@
+<section class="form">
+	<div class="container">
+		<form class="form__form" id="contactForm">
+			<h3 class="form__form-title">Свяжитесь с нами</h3>
+			<p class="form__form-text">Отправьте заявку, чтобы получить консультацию</p>
+			<div class="form__form-row">
+				<div class="form__form-group">
+					<input type="text" class="form__form-input" name="name" placeholder="Имя" required>
+				</div>
+				<div class="form__form-group">
+					<input type="tel" class="form__form-input" name="phone" placeholder="Телефон" required>
+				</div>
+				<div class="form__form-group">
+					<input type="mail" class="form__form-input" name="email" placeholder="Почта" required>
+				</div>
+				<button type="submit" class="form__form-submit button button--accent">Зарегистрироваться</button>
+			</div>
+			
+			
+			<label class="form__checkbox">
+				<input type="checkbox" class="form__checkbox-input" name="agreement" required>
+				<span class="form__checkbox-text">Я&nbsp;даю согласие на&nbsp;обработку персональных данных, согласен на&nbsp;получение информационных рассылок от&nbsp;&laquo;Synergy Travel &raquo; и&nbsp;соглашаюсь c&nbsp;политикой конфиденциальности</span>
+			</label>
+		</form>
+	</div>
+</section> 
