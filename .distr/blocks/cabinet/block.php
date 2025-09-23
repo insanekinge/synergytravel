bed6411529404e48321657c79160f330240feaa0
Kinge Vlad <VKinge@synergy.ru>
2025-09-23 16:35:20 +0300

    ID-5 add product block

--- .distr/blocks/cabinet/block.php
+++ .distr/blocks/cabinet/block.php
@@ -1,11 +1,11 @@
 <main class="cabinet">
     <div class="container">
 
-        <div class="cobinet__top">
-            <a href="/" class="cobinet__top-back"><img src="img/register/arrow-back.svg" alt="" class="register__header-back_img">Вернуться на главную</a>
+        <div class="cabinet__top">
+            <a href="/" class="cabinet__top-back"><img src="img/register/arrow-back.svg" alt="" class="cabinet__top-back_img">Вернуться на главную</a>
             
             <h1 class="cabinet__top-title">
-                Добро пожаловать в личный кабинет, <span class="cabinet__top-title_name">Юлия</span>!
+                Добро пожаловать <br>в личный кабинет, <span class="cabinet__top-title_name">Юлия</span>!
             </h1>
         </div>
 
@@ -21,11 +21,11 @@
                     </div>
                     <div class="cabinet__main-side_unique-bottom">
 
-                        <p class="cabinet__main-side_unique-bottom_description"></p>
+                        <p class="cabinet__main-side_unique-bottom_description">Спланируйте поездку идеально походящую именно вам</p>
                         <ul class="cabinet__main-side_unique-list">
-                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/arrow-back.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
-                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/arrow-back.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
-                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/arrow-back.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
+                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/fisrt.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
+                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/second.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
+                            <li class="cabinet__main-side_unique-list_item"><img src="img/cabinet/third.svg" alt="" class="cabinet__main-side_unique-list_item-img"></li>
                         </ul>
                     </div>
 
@@ -45,10 +45,10 @@
 
             <div class="cabinet__main-tour">
                 <div class="cabinet__main-tour_top">
-                    <h2 class="cabinet__main-tour-top_title">Ваши туры</h2>
-                    <div class="cabinet__main-tour-top_option">
-                        <p class="cabinet__main-tour-top_option-text">Спланируйте свое путешествие</p>
-                        <a href="" class="cabinet__main-tour-top_option-button">Смотреть все</a>
+                    <h2 class="cabinet__main-tour_top-title">Ваши туры</h2>
+                    <div class="cabinet__main-tour_top-option">
+                        <p class="cabinet__main-tour_top-option_text">Спланируйте свое путешествие</p>
+                        <a href="" class="cabinet__main-tour_top-option_button">Смотреть все</a>
                     </div>
                 </div>
 
